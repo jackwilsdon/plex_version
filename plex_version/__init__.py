bd9bcba5b2ddfd5bcdf85b1de14313e60756a7bd
Jack Wilsdon <jack.wilsdon@gmail.com>
2016-01-20 15:33:50 +0000

    Add __version__ and __author__ to plex_version

--- plex_version/__init__.py
+++ plex_version/__init__.py
@@ -2,5 +2,8 @@ from .version import PlexVersion
 from .parser import PlexVersionParser
 from .downloader import PlexVersionDownloader, PlexPassVersionDownloader
 
+__version__ = '0.1.0'
+__author__ = 'Jack Wilsdon <jack.wilsdon@gmail.com>'
+
 __all__ = ['PlexVersion', 'PlexVersionParser', 'PlexVersionDownloader',
            'PlexPassVersionDownloader']
