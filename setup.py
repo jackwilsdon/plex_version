da40441546031717ade87d28e50f14065fc8899b
Jack Wilsdon <jack.wilsdon@gmail.com>
2016-11-06 01:16:01 +0000

    Make setup_functions import private

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ Version information is retrieved from https://plex.tv.
 
 from os import path as _path
 import setuptools as _setuptools
-from setup_functions import get_file_content, get_assignment_value
+import setup_functions as _setup_functions
 
 
 setup_directory = _path.dirname(__file__)
@@ -15,9 +15,9 @@ readme_path = _path.join(setup_directory, 'README.rst')
 
 _setuptools.setup(
     name='plex_version',
-    version=get_assignment_value(init_path, '__version__', True),
+    version=_setup_functions.get_assignment_value(init_path, '__version__', True),
     description='Plex Version API',
-    long_description=get_file_content(readme_path),
+    long_description=_setup_functions.get_file_content(readme_path),
     author='Jack Wilsdon',
     author_email='jack.wilsdon@gmail.com',
     url='https://github.com/jackwilsdon/plex_version',
